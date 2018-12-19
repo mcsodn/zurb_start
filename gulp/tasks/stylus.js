6057141895c52cc7cb1ec194ed949129ba45a26c
Maks Oneyear <mcsodn@gmail.com>
2018-12-19 16:28:30 +0500

    add npm gulp-sourcemaps&gulp-notify

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -1,17 +1,15 @@
 module.exports = function () {
 	$.gulp.task('stylus', function () {
-	return $.gulp.src('src/static/stylus/main.styl')
+	return $.gulp.src('src/main.styl')
 		.pipe($.gp.sourcemaps.init())
 		.pipe($.gp.stylus({
 			'include css':true
 		}))
-		
+
 		.on("error", $.gp.notify.onError({	        
 	        title: "style"
-      	}))
-		.pipe($.gp.csso())
-		.pipe($.gp.sourcemaps.write())
-		.pipe($.gulp.dest('build/static/css/'))		
+      	}))		
+		.pipe($.gulp.dest('src/'))		
 		.pipe($.bs.reload({
 			stream:true
 		}));
